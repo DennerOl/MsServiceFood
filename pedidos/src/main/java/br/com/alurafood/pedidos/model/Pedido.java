5afdb39abe5a12db596d929c997ad1167d51e25f
DennerOl <dener155@hotmail.com>
2024-04-11 21:51:06 -0300

    PedidoMS e seu registro no Eureka

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/model/Pedido.java
@@ -0,0 +1,43 @@
+package br.com.alurafood.pedidos.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pedidos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalDateTime dataHora;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "pedido")
+    private List<ItemDoPedido> itens = new ArrayList<>();
+}
