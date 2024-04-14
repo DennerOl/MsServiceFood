e1529aec852ded74549a7d236a4e0423fe6441ee
DennerOl <dener155@hotmail.com>
2024-04-14 11:39:43 -0300

    Implementação Gateway

--- gateway/src/main/java/br/com/food/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/food/gateway/GatewayApplication.java
@@ -1,15 +1,17 @@
-package br.com.food.pagamentos;
+package br.com.food.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
-public class PagamentosApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PagamentosApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
-	// spring.mvc.servlet.load-on-startup=1
+
 }
---
+++ gateway/src/test/java/br/com/food/gateway/GatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package br.com.food.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class GatewayApplicationTests {
+
+}
