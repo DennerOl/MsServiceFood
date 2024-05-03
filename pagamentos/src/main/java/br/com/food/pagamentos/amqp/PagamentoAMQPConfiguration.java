fd5981f0ca8ea1fdbb4a9459b22544dee100333e
DennerOl <dener155@hotmail.com>
2024-05-03 20:17:21 -0300

    PagamentosMs mandando a mensagem em json

--- pagamentos/src/main/java/br/com/food/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ pagamentos/src/main/java/br/com/food/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -29,5 +31,20 @@ public class PagamentoAMQPConfiguration {
   @Bean
   public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
     return event -> rabbitAdmin.initialize();
+  }
+
+  // metodos para enviar as mensagem no formato json
+  @Bean
+  public Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+      Jackson2JsonMessageConverter messageConverter) {
+
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(messageConverter);
+    return rabbitTemplate;
   }
 }
