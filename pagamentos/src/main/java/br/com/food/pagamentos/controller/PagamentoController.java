ed79e0f95c89b37480cb6b9c986cfc56a7956192
DennerOl <dener155@hotmail.com>
2024-05-02 21:42:05 -0300

    Publicando mensagens no RabbitMQ

--- pagamentos/src/main/java/br/com/food/pagamentos/controller/PagamentoController.java
+++ pagamentos/src/main/java/br/com/food/pagamentos/controller/PagamentoController.java
@@ -2,6 +2,8 @@ package br.com.food.pagamentos.controller;
 
 import java.net.URI;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,6 +35,9 @@ public class PagamentoController {
   @Autowired
   private PagamentoService service;
 
+  @Autowired
+  private RabbitTemplate rabbitTemplate;
+
   @GetMapping
   public Page<PagamentoDto> listar(@PageableDefault(size = 10) Pageable paginacao) {
     return service.obterTodos(paginacao);
@@ -49,6 +54,8 @@ public class PagamentoController {
     PagamentoDto pagamento = service.criarPagamento(dto);
     URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
 
+    Message message = new Message(("Criei um pagamento com o id " + pagamento.getId()).getBytes());
+    rabbitTemplate.send("pagamento.concluido", message);
     return ResponseEntity.created(endereco).body(pagamento);
   }
 
