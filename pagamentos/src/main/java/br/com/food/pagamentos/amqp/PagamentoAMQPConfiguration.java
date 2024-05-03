d1766a99e524394ca023b8a4a5f4ddfadc520c37
DennerOl <dener155@hotmail.com>
2024-05-02 21:09:59 -0300

    Criando fila e conexão RabbitMQ

---
+++ pagamentos/src/main/java/br/com/food/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.food.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PagamentoAMQPConfiguration {
+
+  @Bean
+  public Queue criarFila() {
+
+    // return new Queue("pagamento.concluido", false);
+
+    return QueueBuilder.nonDurable("pagamento.concluido").build();
+
+  }
+
+  @Bean
+  public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+    return new RabbitAdmin(conn);
+  }
+
+  @Bean
+  public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
+    return event -> rabbitAdmin.initialize();
+  }
+}
--- pedidos/src/test/java/br/com/alurafood/pedidos/PedidosApplicationTests.java
+++ pedidos/src/test/java/br/com/alurafood/pedidos/PedidosApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.alurafood.pedidos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PedidosApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
